40a38d3ff2f759c6ec70e326b0217507bef72adf
Kaustubh Sonawane <sona.om78@gmail.com>
2021-04-29 00:38:37 -0500

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-script_dir = os.path.dirname(os.path.abspath(__file__))
-print(script_dir)
-rel_path = "../MeetingCam/filler/{}{}"
-file_path = os.path.normpath(os.path.join(script_dir, rel_path))
-print(file_path)
